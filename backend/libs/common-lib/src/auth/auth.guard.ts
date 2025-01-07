1bc0dfe82a49ed96c872016a50b5092c7aef2a13
Hernán Salambay <hernansalambay@icloud.com>
2025-01-07 18:34:57 +0100

    fix: Improve error message for missing Bearer token in AuthGuard

--- backend/libs/common-lib/src/auth/auth.guard.ts
+++ backend/libs/common-lib/src/auth/auth.guard.ts
@@ -30,7 +30,7 @@ export class AuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Missing Bearer token');
     }
     try {
       const payload = await this.jwtService.verifyAsync(token, {

f679e86064de7307d7dd95351a8d7e80f11edd52
peedronicolas <pedronicolas62@gmail.com>
2025-01-09 18:51:02 +0100

    feat: add community statistics registration and event listener for community actions

--- backend/apps/statistics-ms/src/platform-statistics/community-statistics/application/community-statistics.service.ts
+++ backend/apps/statistics-ms/src/platform-statistics/community-statistics/application/community-statistics.service.ts
@@ -4,4 +4,21 @@ export abstract class CommunityStatisticsService {
   abstract getAll(): Promise<CommunityStatistics[]>;
 
   abstract getTotalSupports(): Promise<number>;
+
+  abstract registerComunityCreation(
+    communityId: string,
+    communityName: string,
+  ): Promise<void>;
+
+  abstract registerCauseSupport(communityId: string): Promise<void>;
+
+  abstract registerCausesTargeted(
+    communityId: string,
+    amount: number,
+  ): Promise<void>;
+
+  abstract registerCausesAchieved(
+    communityId: string,
+    amount: number,
+  ): Promise<void>;
 }

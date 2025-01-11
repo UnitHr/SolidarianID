11575af333a352b45cc0d78f70c306f6ffda280b
aurora2323 <4auroralo@gmail.com>
2025-01-11 16:32:34 +0100

    Fixed cypress config

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,9 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cypress_1 = require("cypress");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const cypress_1 = require('cypress');
 exports.default = (0, cypress_1.defineConfig)({
-    e2e: {
-        baseUrl: 'http://localhost:3005',
-    },
+  e2e: {
+    baseUrl: 'http://localhost:3005',
+  },
 });
 //# sourceMappingURL=cypress.config.js.map

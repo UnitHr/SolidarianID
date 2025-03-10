fb3762ce3da9f0123746baa5a104f734a2f48841
Dongyue Yu <56863849+ydyydy@users.noreply.github.com>
2025-03-10 19:21:15 +0100

    feat: remove frontend

--- frontend/cypress.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const cypress_1 = require('cypress');
-exports.default = (0, cypress_1.defineConfig)({
-  e2e: {
-    baseUrl: 'http://localhost:3005',
-  },
-});
-//# sourceMappingURL=cypress.config.js.map

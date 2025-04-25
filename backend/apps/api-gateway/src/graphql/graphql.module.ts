a84c695d689c1681b5b4a6706be060772fbfecae
Hernán Salambay <hernansalambay@icloud.com>
2025-04-25 11:43:53 +0200

    feat: add community model, resolver, and service with GraphQL integration

--- backend/apps/api-gateway/src/graphql/graphql.module.ts
+++ backend/apps/api-gateway/src/graphql/graphql.module.ts
@@ -1,22 +1,36 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { HttpModule } from '@nestjs/axios';
 import { join } from 'path';
 import { UserResolver } from './resolvers/user.resolver';
 import { FollowerResolver } from './resolvers/follower.resolver';
+import { CommunityResolver } from './resolvers/community.resolver';
 import { UserService } from './services/user.service';
+import { CommunityService } from './services/community.service';
 import { DateScalar } from './scalars/date.scalar';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'schema.gql'),
+      autoSchemaFile: join(
+        process.cwd(),
+        '/apps/api-gateway/src/graphql/schema.gql',
+      ),
       sortSchema: true,
       playground: true,
       introspection: true,
     }),
+    HttpModule,
   ],
-  providers: [UserResolver, FollowerResolver, UserService, DateScalar],
+  providers: [
+    UserResolver,
+    FollowerResolver,
+    CommunityResolver,
+    UserService,
+    CommunityService,
+    DateScalar,
+  ],
 })
 export class GraphQLAppModule {}

9881c675428672d45089b8a8065173cf955c3b4a
aurora2323 <4auroralo@gmail.com>
2025-04-25 23:43:19 +0200

    navigate to notifications view when clicking on a push notification

--- frontend/public/javascripts/sw.js
+++ frontend/public/javascripts/sw.js
@@ -1,20 +1,20 @@
-"use strict";
+'use strict';
 
-var version = "v1";
-const currentCache = "cache-" + version;
+var version = 'v1';
+const currentCache = 'cache-' + version;
 
 const files = [
-  "https://code.jquery.com/jquery-3.5.1.slim.min.js",
-  "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js",
-  "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js",
-  "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css",
-  "/shell.html",
-  "/stylesheets/style.css",
-  "/javascripts/main.js",
-  "/javascripts/sw.js"
+  'https://code.jquery.com/jquery-3.5.1.slim.min.js',
+  'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
+  '/shell.html',
+  '/stylesheets/style.css',
+  '/javascripts/main.js',
+  '/javascripts/sw.js',
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(currentCache).then((cache) => {
       return cache.addAll(files);
@@ -22,64 +22,49 @@ self.addEventListener("install", (event) => {
   );
 });
 
-self.addEventListener("activate", function (event) {
+self.addEventListener('activate', function (event) {
   event.waitUntil(
     caches.keys().then((cacheNames) =>
       Promise.all(
         cacheNames
-          .map((c) => c.split("-"))
-          .filter((c) => c[0] === "cache")
+          .map((c) => c.split('-'))
+          .filter((c) => c[0] === 'cache')
           .filter((c) => c[1] !== version)
-          .map((c) => caches.delete(c.join("-")))
+          .map((c) => caches.delete(c.join('-')))
       )
     )
   );
 });
 
-//self.addEventListener("install", (event) => {
-//  event.waitUntil()})
-
-//self.addEventListener("activate", () => self.clients.claim());
-
-/*self.addEventListener("fetch", function (event) {
-  event.respondWith(fetch(event.request));
-  //event.respondWith(fetch('/test.html'));
-});*/
-
-self.addEventListener("fetch", function (event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
-      console.log(
-        event.request.url + " " + (response ? "in cache" : "not in cache")
-      );
+      console.log(event.request.url + ' ' + (response ? 'in cache' : 'not in cache'));
       return response || fetch(event.request);
     })
   );
 });
 
-// mostrar notificaciones
-/* self.addEventListener("push", function (event) {
-  const payload = event.data ? event.data.text() : "no payload";
-  event.waitUntil(
-    self.registration.showNotification("ServiceWorker Cookbook", {
-      body: payload,
-    }),
-  );
-}); */
-
 // Register event listener for the 'push' event.
-self.addEventListener('push', function(event) {
-  // Retrieve the textual payload from event.data (a PushMessageData object).
-  // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
-  // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
+self.addEventListener('push', function (event) {
   const payload = event.data ? event.data.text() : 'no payload';
 
   // Keep the service worker alive until the notification is created.
   event.waitUntil(
-    // Show a notification with title 'ServiceWorker Cookbook' and use the payload
-    // as the body.
-    self.registration.showNotification('ServiceWorker Cookbook', {
+    self.registration.showNotification('New notification from SolidarianID', {
       body: payload,
+      data: { url: 'http://localhost:5173/notifications' },
     })
+  );
+});
+
+self.addEventListener('notificationclick', function (event) {
+  let urlToOpen = event.notification.data.url;
+
+  console.log('URL to open:', urlToOpen); // Muestra la URL en la consola para depuración.
+
+  // Usamos waitUntil para abrir la ventana en el cliente.
+  event.waitUntil(
+    clients.openWindow(urlToOpen) // Abre la URL en la ventana activa del cliente.
   );
 });

13d922a276a7275f35558080e9da37b298868039
aurora2323 <4auroralo@gmail.com>
2025-04-27 11:57:27 +0200

    minor changes

--- backend/apps/users-ms/src/notifications/infra/push-notification.service.ts
+++ backend/apps/users-ms/src/notifications/infra/push-notification.service.ts
@@ -4,46 +4,26 @@ import { Injectable, Logger } from '@nestjs/common';
 export class PushNotificationService {
   private readonly logger = new Logger(PushNotificationService.name);
 
-  // eslint-disable-next-line consistent-return
-  private async getUserSubscription(userId: string) {
-    try {
-      const subscriptionResponse = await fetch(
-        `${process.env.PUSH_SERVER_URL}/subscription/${userId}`,
-      );
-
-      if (subscriptionResponse.ok) {
-        this.logger.log(`Checking subscription for user ID: ${userId}.`);
-        return await subscriptionResponse.json();
-      }
-    } catch (error) {
-      this.logger.error(
-        `Error getting subscription for user ID: ${userId}.`,
-        error,
-      );
-      return null;
-    }
-  }
-
   async sendPushNotification(userId: string): Promise<void> {
-    const subscription = await this.getUserSubscription(userId);
-
-    if (subscription.isSubscribed) {
-      try {
-        await fetch(`${process.env.PUSH_SERVER_URL}/sendNotification`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          credentials: 'include',
-          body: JSON.stringify({
-            subscription: subscription.subscription,
-            payload: `New notification`,
-            ttl: 86400,
+    try {
+      await fetch(`${process.env.PUSH_SERVER_URL}/sendNotification`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        body: JSON.stringify({
+          userId,
+          payload: JSON.stringify({
+            title: 'New Notification',
+            body: 'You have a new notification.',
           }),
-        });
-      } catch (error) {
-        this.logger.error(`Error sending push notification.`, error);
-      }
+          ttl: 86400,
+          delay: 0,
+        }),
+      });
+    } catch (error) {
+      this.logger.error(`Error sending push notification.`, error.message);
     }
   }
 }

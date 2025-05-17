e30dcaa66e03d012af950204735ff97abe8c270b
amt-um <a.montoyatoro@um.es>
2025-05-17 19:11:07 +0200

    Advances in ej1 dstilh

--- backend/dstilh/ej1/main.py
+++ backend/dstilh/ej1/main.py
@@ -1,6 +1,11 @@
 import pymongo
 import psycopg2
 import fasttext
+import argparse
+import sys
+from transformers import BertTokenizer, BertModel
+import torch
+import numpy as np
 from rich.console import Console
 from rich.progress import track
 from flask import Flask, jsonify, request
@@ -13,7 +18,9 @@ console = Console()
 mongo_client = None
 postgres_client = None
 causes_se = None
-fasttext_model = None
+se_model = None
+args = None
+tokenizer = None
 
 def update_we_causes():
     global causes_se
@@ -31,7 +38,7 @@ def update_we_causes():
         community = communities_collection.find_one({"id": cause["communityId"]})
         if community:
             cause_string = cause["description"] + "*" + community["description"]
-            causes_se[cause["id"]] = fasttext_model.get_sentence_vector(cause_string)
+            causes_se[cause["id"]] = get_sentence_embedding(cause_string)
         else:
             console.log(f"[update_we_causes] [bold red]Community not found for cause {cause['id']}[/bold red]")
     
@@ -40,7 +47,7 @@ def update_we_causes():
 def print_all_users_id():
 
     cursor = postgres_client.cursor(cursor_factory=RealDictCursor)
-    cursor.execute("SELECT * FROM user")
+    cursor.execute("SELECT * FROM public.user")
     users = cursor.fetchall()
 
     if users:
@@ -55,14 +62,15 @@ def get_user_string(user_id):
 
     user_string = ""
 
-    cursor = postgres_client.cursor()
-    cursor.execute("SELECT * FROM user WHERE id = %s", (user_id,))
+    cursor = postgres_client.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("SELECT * FROM public.user WHERE id = %s", (user_id,))
     user = cursor.fetchone()
     cursor.close()
 
     if user:
-        console.log(f"[get_user_string] User found: {user['name']}")
-        user_string = user['description']
+        console.log(f"[get_user_string] User found: {user['firstName']} {user['lastName']}")
+        user_string = user['bio']
+        console.log(f"[get_user_string] User bio: {user_string}")
 
         causes_collection = mongo_client["causes"]
         console.log("[get_user_string] Fetching causes...")
@@ -115,18 +123,19 @@ def get_similar_causes():
     user_string = get_user_string(user_id)
 
     if user_string:
-        user_se = fasttext_model.get_sentence_vector(user_string)
+        user_se = get_sentence_embedding(user_string)
 
         similarities = []
         for cause_id, cause_se in causes_se.items():
-            similarity = cosine_similarity([user_se], [cause_se])[0][1]
+            similarity = get_similarity(user_se, cause_se)
+            console.log(f"[endpoint /causes] Similarity for cause {cause_id}: {similarity}")
             similarities.append((cause_id, similarity))
 
         similarities.sort(key=lambda x: x[1], reverse=True)
-        top_causes = similarities[:5]
+        top_causes = similarities[:3]
 
         cause_ids = [cause_id for cause_id, _ in top_causes]
-        console.log(f"[endpoint /causes] Top 5 causes for user {user_id}: {cause_ids}")
+        console.log(f"[endpoint /causes] Top 3 causes for user {user_id}: {cause_ids}")
 
         return jsonify({"causeIds": cause_ids})
     else:
@@ -136,8 +145,37 @@ def get_similar_causes():
 def setup():
     global mongo_client
     global postgres_client
-    global fasttext_model
+    global se_model
     global causes_se
+    global tokenizer
+    global args
+
+    # Extract command line arguments
+    parser = argparse.ArgumentParser(description="Select FastText or BERT model")
+    parser.add_argument('--model', type=str, choices=['fasttext', 'bert'], default='bert', help='Model to use for embeddings')
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        console.log("[bold red]No model selected. Please select either FastText or BERT[/bold red]")
+        return False
+
+    if(args.model == 'fasttext'):
+        try:
+            se_model = fasttext.load_model ("embeddings-s-model.bin")
+            console.log("Loaded FastText model")
+        except Exception as e:
+            console.log(f"[bold red]Error loading FastText model: {e}")
+            return False
+    
+    elif(args.model == 'bert'):
+        try:
+            tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+            se_model = BertModel.from_pretrained('bert-base-uncased')
+            console.log("Loaded BERT model")
+        except Exception as e:
+            console.log(f"[bold red]Error loading BERT model: {e}")
+            return False
+
 
     # Connect to MongoDB
     try:
@@ -161,18 +199,12 @@ def setup():
         console.log(f"[bold red]Error connecting to PostgreSQL: {e}")
         return False
     
-    # Load FastText model
-    try:
-        fasttext_model = fasttext.load_model ("embeddings-s-model.bin")
-        console.log("Loaded FastText model")
-    except Exception as e:
-        console.log(f"[bold red]Error loading FastText model: {e}")
-        return False
     
     # Initialize we_causes
     try:
         causes_se = {}
         update_we_causes()
+        print(type(causes_se))
     except Exception as e:
         console.log(f"[bold red]Error initializing causes_we: {e}")
         return False
@@ -181,6 +213,30 @@ def setup():
     
     return True
 
+def get_sentence_embedding(text):
+
+    global se_model
+    global tokenizer
+
+    if args.model == 'fasttext':
+        return se_model.get_sentence_vector(text)
+    
+    elif args.model == 'bert':
+        inputs = tokenizer(text, return_tensors='pt')
+        with torch.no_grad():
+            outputs = se_model(**inputs)
+        last_hidden_states = outputs.last_hidden_state
+        sentence_embedding = torch.mean(last_hidden_states, dim=1).numpy()
+        return sentence_embedding
+    
+def get_similarity(user_se, cause_se):
+    global args
+
+    if args.model == 'fasttext':
+        return cosine_similarity([user_se], [cause_se])[0][0]
+    
+    elif args.model == 'bert':
+        return cosine_similarity(user_se, cause_se)[0][0]
 
 if __name__ == '__main__':  
     if setup():
